be95521892809a70eaad3693365163d4493bded1
Laedd <156594257+Sauqyyr@users.noreply.github.com>
2024-03-05 09:31:26 +0700

    Item.java

---
+++ Item.java
@@ -0,0 +1,22 @@
+public class ItemLaundry {
+    private String jenis;
+    private int jumlah;
+
+    public ItemLaundry(String jenis, int jumlah) {
+        this.jenis = jenis;uang
+        this.jumlah = jumlah;100000000000000000000000  
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void displayInfo() {
+        System.out.println("Jenis Item: " + jenis);
+        System.out.println("Jumlah: " + jumlah);
+    }
+}
